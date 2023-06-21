67ae3ad9b6426d63ebfe9a21ae9423aefdcdd74f
Claire Park <giyami0425@gmail.com>
2023-06-21 20:06:47 +0900

    리액트 기초 섹션 완료

---
+++ pages/01-02-emotion/index.js
@@ -0,0 +1,14 @@
+import { Email, EmailInput } from "../../styles/emotion";
+
+export default function EmotionPage() {
+
+  // Javascript 
+
+  return (
+    <div>
+      <Email>이메일</Email>
+      <EmailInput type="text" />
+      <button>클릭하세요</button>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+// 모든 페이지의 공통적으로 적용되는 설정파일
